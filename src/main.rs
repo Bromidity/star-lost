ca4cd4c108eca8473ba6960c171019b66a826af8
Mathias Pius <contact@pius.io>
2022-08-12 16:36:00 +0200

    Leftover from migration to 0.8

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     tests::route::spawn_route_ship(&mut commands, &asset_server, vec![a.into(), b.into()]);
 
     tests::controls::spawn_player_ship(&mut commands, asset_server);
-    //commands.spawn_bundle(UiCameraBundle::default());
 
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
